2eabab0c61911d8797e747ca9d4fd0ae14892777
ahmedmellouli <ahmed.mellouli@esprit.tn>
2024-01-29 07:24:56 -0800

    front design completed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,6 +1,7 @@
 import "./rightbar.css"
 import { Users } from "../../dummyData";
 import Online from "../Online/Online";
+import Home from "../../pages/home/Home";
 
 
 export default function Rightbar({profile}) {
@@ -22,12 +23,57 @@ export default function Rightbar({profile}) {
     )
   };
   const ProfileRightbar = () =>{
-    return(<h1>hey, it's my profile</h1>)
+    return(
+      <>
+      <h4 className="rightbarTitle">Ma3loumet, sidek el hendi</h4>
+      <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">City:</span>
+          <span className="rightbarInfoValue">El hend bel tbi3a</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">From:</span>
+          <span className="rightbarInfoValue">El hend</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Relationship:</span>
+          <span className="rightbarInfoValue">C compliqué 5ouya</span>
+        </div>
+      </div>
+      <h4 className="rightbarTitle">User friends</h4>
+      <div className="rightbarFollowings">
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Cena</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Cena</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Cena</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Cena</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Cena</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">John Cena</span>
+        </div>
+      </div>
+      </>
+    )
   }
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-          <ProfileRightbar/>
+          {profile ? <ProfileRightbar/> : <HomeRightbar/>}
       </div>
     </div>
   );
